edfc8f763bc07dd164b28ddcca9df4955ee6bc29
Gharbi Mohammed <mgharbik@gmail.com>
2017-10-15 13:17:28 +0200

    displaying appointments at the root page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      appointments: []
+    }
+  }
+
+  componentDidMount() {
+    $.ajax({
+      type: 'GET',
+      url: 'http://localhost:3001/appointments',
+    }).done(data => {
+      this.setState({appointments: data});
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +28,9 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.appointments.map(appointment => {
+            return <p>{appointment.title}</p>;
+          })}
         </p>
       </div>
     );
