c6a2dfd2a367d9b1573ab39c485c2a435166516d
Gharbi Mohammed <mgharbik@gmail.com>
2017-10-16 00:38:53 +0200

    adding the login page and fetching authenticated user data

--- src/components/AppointmentForm.js
+++ src/components/AppointmentForm.js
@@ -39,7 +39,8 @@ export default class AppointmentForm extends React.Component {
       $.ajax({
         type: 'GET',
         url: `http://localhost:3001/appointments/${this.props.match.params.id}`,
-        dataType: 'JSON'
+        dataType: 'JSON',
+        headers: JSON.parse(sessionStorage.getItem('user')),
       }).done((data) => {
         this.setState({
           title: {value: data.title, valid: true},
@@ -93,16 +94,19 @@ export default class AppointmentForm extends React.Component {
 
   addAppointment () {
     const appointment = {title: this.state.title.value, appt_time: this.state.appt_time.value}
-    $.post('http://localhost:3001/appointments',
-      {appointment: appointment})
-      .done((data) => {
-        this.props.handleNewAppointment(data);
-        this.resetFormErrors();
-      })
-      .fail((response) => {
-        this.setState({formErrors: response.responseJSON,
-                        formValid: false})
-      });
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:3001/appointments',
+      data: {appointment: appointment},
+      headers: JSON.parse(sessionStorage.getItem('user')),
+    })
+    .done((data) => {
+      this.props.handleNewAppointment(data);
+      this.resetFormErrors();
+    })
+    .fail((response) => {
+      this.setState({formErrors: response.responseJSON, formValid: false})
+    });
   }
 
   updateAppointment () {
@@ -110,7 +114,8 @@ export default class AppointmentForm extends React.Component {
     $.ajax({
       type: 'PATCH',
       url: `http://localhost:3001/appointments/${this.props.match.params.id}`,
-      data: {appointment: appointment}
+      data: {appointment: appointment},
+      headers: JSON.parse(sessionStorage.getItem('user')),
     })
     .done((data) => {
       console.log('appointment updated!');
@@ -131,6 +136,7 @@ export default class AppointmentForm extends React.Component {
       $.ajax({
         type: 'DELETE',
         url: `http://localhost:3001/appointments/${this.props.match.params.id}`,
+        headers: JSON.parse(sessionStorage.getItem('user')),
       })
       .done((data) => {
         this.props.history.push('/');
